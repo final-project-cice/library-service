c939783ad1612fb5ffe54a423cbd2c742c5ff461
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-10-22 12:46:38 +0200

    Adding functionality to classes: UserService, UserServiceImpl.

--- src/main/java/com/trl/libraryservice/service/UserService.java
+++ src/main/java/com/trl/libraryservice/service/UserService.java
@@ -1,5 +1,40 @@
 package com.trl.libraryservice.service;
 
+import com.trl.libraryservice.controller.dto.UserDTO;
+import com.trl.libraryservice.exception.InvalidArgumentException;
+import com.trl.libraryservice.exception.UserWithValueNotExistException;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface UserService {
 
+    UserDTO create(UserDTO userDTO) throws Exception;
+
+
+    UserDTO updateFirstName(Long id, String firstName) throws Exception;
+
+    UserDTO updateLastName(Long id, String lastName) throws Exception;
+
+    UserDTO updateEmail(Long id, String email) throws Exception;
+
+    UserDTO updateBirthday(Long id, LocalDate birthday) throws Exception;
+
+
+    Boolean delete(Long id) throws UserWithValueNotExistException, InvalidArgumentException;
+
+
+    UserDTO findById(Long id) throws Exception;
+
+    List<UserDTO> findByFirstName(String firstName) throws Exception;
+
+    List<UserDTO> findByLastName(String lastName) throws Exception;
+
+    List<UserDTO> findByFirstNameAndLastName(String firstName, String lastName) throws Exception;
+
+    UserDTO findByEmail(String email) throws Exception;
+
+    List<UserDTO> findByBirthday(LocalDate birthday) throws Exception;
+
+    List<UserDTO> findAll() throws Exception;
 }
