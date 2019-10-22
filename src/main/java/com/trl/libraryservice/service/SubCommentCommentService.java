35f1b6ed92405742a3fd1c714bb0992949810586
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-10-22 14:42:28 +0200

    Definition methods in: SubCommentCommentService, SubCommentCommentServiceImpl.

--- src/main/java/com/trl/libraryservice/service/SubCommentCommentService.java
+++ src/main/java/com/trl/libraryservice/service/SubCommentCommentService.java
@@ -1,5 +1,33 @@
 package com.trl.libraryservice.service;
 
+import com.trl.libraryservice.controller.dto.SubCommentCommentDTO;
+import com.trl.libraryservice.controller.dto.UserDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SubCommentCommentService {
 
+    SubCommentCommentDTO create(SubCommentCommentDTO subCommentCommentDTO);
+
+
+    SubCommentCommentDTO updateUser(Long id, UserDTO userDTO);
+
+    SubCommentCommentDTO updateText(Long id, String text);
+
+    SubCommentCommentDTO updateDate(Long id, LocalDate date);
+
+
+    Boolean delete(Long id);
+
+
+    SubCommentCommentDTO findById(Long id);
+
+    List<SubCommentCommentDTO> findByUser(UserDTO userDTO);
+
+    List<SubCommentCommentDTO> findByText(String text);
+
+    List<SubCommentCommentDTO> findByDate(LocalDate date);
+
+    List<SubCommentCommentDTO> findAll();
 }
--- src/main/java/com/trl/libraryservice/service/impl/SubCommentCommentServiceImpl.java
+++ src/main/java/com/trl/libraryservice/service/impl/SubCommentCommentServiceImpl.java
@@ -1,14 +1,73 @@
 package com.trl.libraryservice.service.impl;
 
+import com.trl.libraryservice.controller.dto.SubCommentCommentDTO;
+import com.trl.libraryservice.controller.dto.UserDTO;
 import com.trl.libraryservice.service.SubCommentCommentService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class SubCommentCommentServiceImpl implements SubCommentCommentService {
 
     private static final Logger LOG = LoggerFactory.getLogger(SubCommentCommentServiceImpl.class);
 
+    @Override
+    public SubCommentCommentDTO create(SubCommentCommentDTO subCommentCommentDTO) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public SubCommentCommentDTO updateUser(Long id, UserDTO userDTO) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public SubCommentCommentDTO updateText(Long id, String text) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public SubCommentCommentDTO updateDate(Long id, LocalDate date) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public SubCommentCommentDTO findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<SubCommentCommentDTO> findByUser(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public List<SubCommentCommentDTO> findByText(String text) {
+        return null;
+    }
+
+    @Override
+    public List<SubCommentCommentDTO> findByDate(LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public List<SubCommentCommentDTO> findAll() {
+        return null;
+    }
 }
