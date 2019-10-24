3b4f556d38d1c658c860081449315a450e39e8bf
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-10-24 12:47:07 +0200

    Definition methods in: CommentBookService, CommentBookServiceImpl.

--- src/main/java/com/trl/libraryservice/service/CommentBookService.java
+++ src/main/java/com/trl/libraryservice/service/CommentBookService.java
@@ -1,5 +1,37 @@
 package com.trl.libraryservice.service;
 
+import com.trl.libraryservice.controller.dto.CommentBookDTO;
+import com.trl.libraryservice.controller.dto.SubCommentCommentDTO;
+import com.trl.libraryservice.controller.dto.UserDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CommentBookService {
 
+
+    CommentBookDTO create(CommentBookDTO commentBookDTO);
+
+
+    CommentBookDTO updateUser(Long id, UserDTO userDTO);
+
+    CommentBookDTO updateText(Long id, String text);
+
+    CommentBookDTO updateDate(Long id, LocalDate date);
+
+    List<SubCommentCommentDTO> updateSubComment(Long id, SubCommentCommentDTO subCommentCommentDTO);
+
+
+    Boolean delete(Long id);
+
+
+    CommentBookDTO findById(Long id);
+
+    List<CommentBookDTO> findByUser(UserDTO userDTO);
+
+    List<CommentBookDTO> findByText(String text);
+
+    List<CommentBookDTO> findByDate(LocalDate date);
+
+    List<CommentBookDTO> findAll();
 }
--- src/main/java/com/trl/libraryservice/service/impl/CommentBookServiceImpl.java
+++ src/main/java/com/trl/libraryservice/service/impl/CommentBookServiceImpl.java
@@ -1,14 +1,79 @@
 package com.trl.libraryservice.service.impl;
 
+import com.trl.libraryservice.controller.dto.CommentBookDTO;
+import com.trl.libraryservice.controller.dto.SubCommentCommentDTO;
+import com.trl.libraryservice.controller.dto.UserDTO;
 import com.trl.libraryservice.service.CommentBookService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class CommentBookServiceImpl implements CommentBookService {
 
     private static final Logger LOG = LoggerFactory.getLogger(CommentBookServiceImpl.class);
 
+    @Override
+    public CommentBookDTO create(CommentBookDTO commentBookDTO) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public CommentBookDTO updateUser(Long id, UserDTO userDTO) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public CommentBookDTO updateText(Long id, String text) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public CommentBookDTO updateDate(Long id, LocalDate date) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public List<SubCommentCommentDTO> updateSubComment(Long id, SubCommentCommentDTO subCommentCommentDTO) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public CommentBookDTO findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CommentBookDTO> findByUser(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public List<CommentBookDTO> findByText(String text) {
+        return null;
+    }
+
+    @Override
+    public List<CommentBookDTO> findByDate(LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public List<CommentBookDTO> findAll() {
+        return null;
+    }
 }
