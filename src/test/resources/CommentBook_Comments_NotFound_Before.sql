7222e4da39194af4c76500642a06c848f5450487
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-12-12 11:31:09 +0100

    Refactoring...

--- src/test/resources/CommentBook_Comments_NotFound_Before.sql
+++ src/test/resources/CommentBook_Comments_NotFound_Before.sql
@@ -1,102 +1,102 @@
 -- ======================================================================================================== Clear Tables
-DELETE
-FROM author_book;
-DELETE
-FROM book_author;
+delete
+from email_publishing_house;
+delete
+from phone_number_publishing_house;
+delete
+from address_publishing_house;
+delete
+from publishing_house;
 
 
 
-DELETE
-FROM phone_number_author;
-DELETE
-FROM genre_author;
-DELETE
-FROM email_author;
-DELETE
-FROM address_author;
-DELETE
-FROM author;
+
+delete
+from comment_book;
+
+delete
+from sub_comment_comment;
+
+delete
+from genre_book;
 
 
 
-DELETE
-FROM genre_book;
 
-DELETE
-FROM sub_comment_comment;
+delete
+from address_author;
 
-DELETE
-FROM comment_book;
+delete
+from email_author;
+delete
+from genre_author;
+delete
+from phone_number_author;
 
-DELETE
-FROM book;
+delete
+from author;
 
 
+delete
+from author_book;
+delete
+from book_author;
 
-DELETE
-FROM phone_number_publishing_house;
-
-DELETE
-FROM email_publishing_house;
-
-DELETE
-FROM publishing_house;
-
-DELETE
-FROM address_publishing_house;
+delete
+from book;
 
 -- =====================================================================================================================
-INSERT INTO address_publishing_house (id, city, country, house_number, postcode, street)
-VALUES (1, 'Madrid', 'Spain', '1A', 1111, 'Calle Uno');
-INSERT INTO publishing_house (id, name, address_id)
-VALUES (1, 'Publication House', 1);
-INSERT INTO email_publishing_house (id, email, email_type, publishing_house_id)
-VALUES (1, 'email_1_publicationHouse9@email.com', 'Office', 1);
-INSERT INTO email_publishing_house (id, email, email_type, publishing_house_id)
-VALUES (2, 'email_2_publicationHouse9@email.com', 'Office 2', 1);
-INSERT INTO phone_number_publishing_house (id, country_code, phone_number, type, publishing_house_id)
-VALUES (1, '11111', '0111111111111111', 'Office', 1);
-INSERT INTO phone_number_publishing_house (id, country_code, phone_number, type, publishing_house_id)
-VALUES (2, '222222', '02222222222222', 'Fax', 1);
+insert into address_publishing_house (id, city, country, house_number, postcode, street)
+values (1, 'Madrid', 'Spain', '1A', 1111, 'Calle Uno');
+insert into publishing_house (id, name, address_id)
+values (1, 'Publication House', 1);
+insert into email_publishing_house (id, email, email_type, publishing_house_id)
+values (1, 'email_1_publicationHouse9@email.com', 'Office', 1);
+insert into email_publishing_house (id, email, email_type, publishing_house_id)
+values (2, 'email_2_publicationHouse9@email.com', 'Office 2', 1);
+insert into phone_number_publishing_house (id, country_code, phone_number, type, publishing_house_id)
+values (1, '11111', '0111111111111111', 'Office', 1);
+insert into phone_number_publishing_house (id, country_code, phone_number, type, publishing_house_id)
+values (2, '222222', '02222222222222', 'Fax', 1);
 
 
 
-INSERT INTO book (id, name, path_file, publication_date, publishing_house_id)
-VALUES (1, 'My first Book', 'path/path/book', '2019-10-30', 1);
+insert into book (id, name, path_file, publication_date, publishing_house_id)
+values (1, 'My first Book', 'path/path/book', '2019-10-30', 1);
 
 -- =====================================================================================================================
 -- ============================================== Comment not found... =================================================
 -- =====================================================================================================================
 
-INSERT INTO genre_book (id, name, book_id)
-VALUES (1, 'Genre Book Uno', 1);
-INSERT INTO genre_book (id, name, book_id)
-VALUES (2, 'Genre Book Dos', 1);
+insert into genre_book (id, name, book_id)
+values (1, 'Genre Book Uno', 1);
+insert into genre_book (id, name, book_id)
+values (2, 'Genre Book Dos', 1);
 
 
 
-INSERT INTO author (id, birthday, first_name, last_name)
-VALUES (1, '2019-10-30', 'Author_1 FirstName', 'Author_1 LastName');
-INSERT INTO address_author (id, city, country, house_number, postcode, street, author_id)
-VALUES (1, 'Madrid', 'Spain', '1A', 111111, 'Calle Uno', 1);
-INSERT INTO address_author (id, city, country, house_number, postcode, street, author_id)
-VALUES (2, 'Barcelona', 'Spain', '2A', 222222, 'Calle Dos', 1);
-INSERT INTO email_author (id, email, email_type, author_id)
-VALUES (1, 'email_1.author_19@email.com', 'Personal', 1);
-INSERT INTO email_author (id, email, email_type, author_id)
-VALUES (2, 'email_2.author_19@email.com', 'Personal', 1);
-INSERT INTO genre_author (id, name, author_id)
-VALUES (1, 'Genre Uno', 1);
-INSERT INTO genre_author (id, name, author_id)
-VALUES (2, 'Genre Dos', 1);
-INSERT INTO phone_number_author (id, country_code, phone_number, type, author_id)
-VALUES (1, '111', '01111111111111', 'Personal', 1);
-INSERT INTO phone_number_author (id, country_code, phone_number, type, author_id)
-VALUES (2, '222', '0222222222222222', 'Work', 1);
+insert into author (id, birthday, first_name, last_name)
+values (1, '2019-10-30', 'Author_1 FirstName', 'Author_1 LastName');
+insert into address_author (id, city, country, house_number, postcode, street, author_id)
+values (1, 'Madrid', 'Spain', '1A', 111111, 'Calle Uno', 1);
+insert into address_author (id, city, country, house_number, postcode, street, author_id)
+values (2, 'Barcelona', 'Spain', '2A', 222222, 'Calle Dos', 1);
+insert into email_author (id, email, email_type, author_id)
+values (1, 'email_1.author_19@email.com', 'Personal', 1);
+insert into email_author (id, email, email_type, author_id)
+values (2, 'email_2.author_19@email.com', 'Personal', 1);
+insert into genre_author (id, name, author_id)
+values (1, 'Genre Uno', 1);
+insert into genre_author (id, name, author_id)
+values (2, 'Genre Dos', 1);
+insert into phone_number_author (id, country_code, phone_number, type, author_id)
+values (1, '111', '01111111111111', 'Personal', 1);
+insert into phone_number_author (id, country_code, phone_number, type, author_id)
+values (2, '222', '0222222222222222', 'Work', 1);
 
 
 
-INSERT INTO author_book (author_id, book_id)
-VALUES (1, 1);
-INSERT INTO book_author (book_id, author_id)
-VALUES (1, 1);
+insert into author_book (author_id, book_id)
+values (1, 1);
+insert into book_author (book_id, author_id)
+values (1, 1);
--- src/test/resources/CommentBook_Empty_Before.sql
+++ src/test/resources/CommentBook_Empty_Before.sql
@@ -1,45 +1,45 @@
-DELETE
-FROM author_book;
-DELETE
-FROM book_author;
+delete
+from email_publishing_house;
+delete
+from phone_number_publishing_house;
+delete
+from address_publishing_house;
+delete
+from publishing_house;
 
 
 
-DELETE
-FROM phone_number_author;
-DELETE
-FROM genre_author;
-DELETE
-FROM email_author;
-DELETE
-FROM address_author;
-DELETE
-FROM author;
+
+delete
+from comment_book;
+
+delete
+from sub_comment_comment;
+
+delete
+from genre_book;
 
 
 
-DELETE
-FROM genre_book;
 
-DELETE
-FROM sub_comment_comment;
+delete
+from address_author;
 
-DELETE
-FROM comment_book;
+delete
+from email_author;
+delete
+from genre_author;
+delete
+from phone_number_author;
 
-DELETE
-FROM book;
+delete
+from author;
 
 
+delete
+from author_book;
+delete
+from book_author;
 
-DELETE
-FROM phone_number_publishing_house;
-
-DELETE
-FROM email_publishing_house;
-
-DELETE
-FROM publishing_house;
-
-DELETE
-FROM address_publishing_house;
+delete
+from book;
