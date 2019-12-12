f9882e60c659d578edfde85ae366f72779ed511a
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2019-12-12 12:17:55 +0100

    Refactoring...

--- src/test/resources/CommentBook_Empty_Before.sql
+++ src/test/resources/CommentBook_Empty_Before.sql
@@ -1,31 +1,4 @@
 delete
-from email_publishing_house;
-delete
-from phone_number_publishing_house;
-delete
-from address_publishing_house;
-delete
-from publishing_house;
-
-
-
-
-delete
-from comment_book;
-
-delete
-from sub_comment_comment;
-
-delete
-from genre_book;
-
-
-
-
-delete
-from address_author;
-
-delete
 from email_author;
 delete
 from genre_author;
@@ -33,7 +6,7 @@ delete
 from phone_number_author;
 
 delete
-from author;
+from address_author;
 
 
 delete
@@ -41,5 +14,33 @@ from author_book;
 delete
 from book_author;
 
+
+delete
+from author;
+
+
+
+delete
+from genre_book;
+
+delete
+from sub_comment_comment;
+
+delete
+from comment_book;
+
 delete
 from book;
+
+
+
+delete
+from email_publishing_house;
+delete
+from phone_number_publishing_house;
+
+delete
+from publishing_house;
+
+delete
+from address_publishing_house;
--- src/test/resources/SubComment_SubComments_NotFound_Before.sql
+++ src/test/resources/SubComment_SubComments_NotFound_Before.sql
@@ -1,32 +1,5 @@
 -- ========================================================================================================== Clear Tables
 delete
-from email_publishing_house;
-delete
-from phone_number_publishing_house;
-delete
-from address_publishing_house;
-delete
-from publishing_house;
-
-
-
-
-delete
-from comment_book;
-
-delete
-from sub_comment_comment;
-
-delete
-from genre_book;
-
-
-
-
-delete
-from address_author;
-
-delete
 from email_author;
 delete
 from genre_author;
@@ -34,7 +7,7 @@ delete
 from phone_number_author;
 
 delete
-from author;
+from address_author;
 
 
 delete
@@ -42,8 +15,36 @@ from author_book;
 delete
 from book_author;
 
+
+delete
+from author;
+
+
+
+delete
+from genre_book;
+
+delete
+from sub_comment_comment;
+
+delete
+from comment_book;
+
 delete
 from book;
+
+
+
+delete
+from email_publishing_house;
+delete
+from phone_number_publishing_house;
+
+delete
+from publishing_house;
+
+delete
+from address_publishing_house;;
 
 -- =====================================================================================================================
 insert into address_publishing_house (id, city, country, house_number, postcode, street)
